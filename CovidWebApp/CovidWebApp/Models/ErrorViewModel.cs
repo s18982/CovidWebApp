8e2ab1f789c2f4db07a379399802ef9e83e81824
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-25 23:12:26 +0200

    Utworzenie projektu

---
+++ CovidWebApp/CovidWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CovidWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
