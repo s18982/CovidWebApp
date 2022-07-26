e1a9b55521f55565141fcb30c8226e001e6ecdcf
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-26 17:55:49 +0200

    dodanie kontrolera Covid i endpointa covid/index

---
+++ CovidWebApp/CovidWebApp/Controllers/CovidController.cs
@@ -0,0 +1,34 @@
+﻿using CovidWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using CovidWebApp.Data;
+using System.Linq;
+
+namespace CovidWebApp.Controllers
+{
+    public class CovidController : Controller
+    {
+        public async Task<ViewResult> Index()
+        {
+            CountryBase countryBase = new CountryBase();
+            await countryBase.readData("https://api.covid19api.com/summary");
+
+
+            SingleCountryModel[] singleCountryModels = countryBase.GetCountryModel().Countries;
+            var orderedCountryList = from country in singleCountryModels
+                                     orderby country.TotalConfirmed descending
+                                     select country;
+
+            singleCountryModels = orderedCountryList.ToArray();
+            SingleCountryModel[] countryArr = new SingleCountryModel[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                countryArr[i] = singleCountryModels[i];
+            }
+            ViewData["Message"] = countryArr;
+
+            return View();
+        }
+    }
+}
